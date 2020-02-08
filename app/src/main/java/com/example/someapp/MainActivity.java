7e56d8e7b6f93c1a07f50cf08c3088575a0f40a4
jz14918 <jz14918@ic.ac.uk>
2020-02-08 18:01:10 +0000

    setup app skeleton

---
+++ app/src/main/java/com/example/someapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.someapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoNextActivity();
+            }
+        });
+    }
+
+    private void gotoNextActivity() {
+        Intent intent = new Intent(this, InputActivity.class);
+        startActivity(intent);
+    }
+
+}
