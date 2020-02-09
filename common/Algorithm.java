4afe039eb8c1abf9f6ec1caa2b2960aa1a77350f
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2020-02-09 08:46:41 +0000

    Updates

--- common/Algorithm.java
+++ common/Algorithm.java
@@ -2,18 +2,16 @@ package common;
 
 import common.Database;
 
-import java.io.Reader;
-import java.util.Scanner;
-import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 public class Algorithm{
-    private static List<List<String>> ingredients = new ArrayList<>();
-    private static int time;
+    private static Map<String, Double> ingredients = new HashMap<String, Double>();
+    private static int time=0;
     private List<List<String>> result = new ArrayList<>();
 
-    public Algorithm(List<List<String>> a, int b){
+    public Algorithm(Map<String, Double> a, int b){
         ingredients = a;
         time = b;
     }
@@ -23,23 +21,23 @@ public class Algorithm{
         int index=0;
 
         for(int i=0; i<db.getSize(); i++){
-            List<String> recipe = db.getRecipe(i);
-            //System.out.println(recipe.get(2).get(0));
+            Map<String, Double> db_ingredients = new HashMap<String, Double>();
+            db_ingredients = db.getIngredients(i);
 
-            /* for(int j=0; j<recipe.size(); j++){
-                System.out.println(recipe.get(j));
-            } */
-
-            /* if(Integer.parseInt(recipe.get(1)) > ingredients.size()){
-                continue; d
-            } */
-
-            // else{
-            if(recipe.get(2).contains(ingredients.get(0).get(0))){
-                result.add(recipe);
-                index++;
+            boolean isValid = true;
+            Map<String, Double> comp_ingrd = ingredients;
+            for (String key : db_ingredients.keySet()) {
+                if (comp_ingrd.containsKey(key)) {
+                    comp_ingrd.put(key, comp_ingrd.get(key)-db_ingredients.get(key));
+                    if (comp_ingrd.get(key) < 0) {
+                        isValid = false;
+                        break;
+                    }
+                }
             }
-            // }
+            if (isValid) {
+                // pass to jenny
+            }
         }
     }
 
@@ -50,34 +48,9 @@ public class Algorithm{
     }
 
     public static void main(String[] args){
-        List<List<String>> input = new ArrayList<>();
-        List<String> tmp = new ArrayList<>();
-        String line;
-        int time = 1;
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter ingredient");
-        tmp.add(in.nextLine());
-        System.out.println("Enter quantity");
-        tmp.add(in.nextLine());
-
-        while(true){
-            if(!in.hasNextLine()){
-                break;
-            }
-
-            List<String> temp = new ArrayList<>();
-
-            System.out.println("Enter ingredient");
-            temp.add(in.nextLine());
-            System.out.println("Enter quantity");
-            temp.add(in.nextLine());
-
-            input.add(tmp);
-        }
-
-        Algorithm A = new Algorithm(input, time);
+        Map<String, Double> a = new HashMap<String, Double>();
+        int b = 0;
+        Algorithm A = new Algorithm(a, b);
         A.run();
-        A.print();
     }
 }
--- common/Database.java
+++ common/Database.java
@@ -1,5 +1,7 @@
 package common;
 
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
@@ -8,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
 
 //import org.json.simple.*;
 import org.json.simple.JSONArray;
@@ -18,15 +21,50 @@ import org.json.simple.parser.ParseException;
 // title, time, ingredients, instructions, image
 
 public class Database{
-    private List<List<String>> recipes = new ArrayList<>();
+    private JSONObject recipes = new JSONObject();
+    private List<String> name = new ArrayList<>();
+    private List<Integer> time = new ArrayList<>();
+    private List<Map<String, Double>> ingredients = new ArrayList<>();
+    private List<List<String>> instructions = new ArrayList<>();
+    private List<String> image = new ArrayList<>();
 
     public Database(){
         JSONParser jsonParser = new JSONParser();
 
-        try(FileReader reader = new FileReader("*.json")){
-            Object obj = jsonParser.parse(reader);
-            JSONArray recipes = (JSONArray) obj;
-            System.out.println(recipes);
+        try{
+            Object obj = jsonParser.parse(new FileReader("/Users/cwlroda/Desktop/ICHack20/test.json"));
+            recipes = (JSONObject) obj;
+
+            //for(int i=6663; i<24180; i++){
+                JSONObject tmp = (JSONObject)recipes.get(Integer.toString(6663));
+                name.add(String.valueOf(tmp.get("Name")));
+                JSONObject tmp2 = (JSONObject)tmp.get("Ingredients");
+                //Map<String, Double> tmp3 = new HashMap<>();
+                System.out.println(tmp2);
+                List<String> l = new ArrayList<>();
+
+                String[] keys = JSONObject.getNames(tmp2);
+
+                for(String key : tmp2.keySet()){
+
+                }
+                l.add(ArrayList.valueOf(tmp2.get(key)))
+                /* JSONObject tmp3 = (JSONObject)tmp2.get("q1");
+                String tmp4 = String.valueOf(tmp3.get("question"));
+                System.out.println(tmp4); */
+            //}
+
+            //for(int i=0; i<name.size(); i++){
+            System.out.println(name.get(0));
+            //}
+
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(ParseException e){
+            e.printStackTrace();
         } catch(Exception e){
             e.printStackTrace();
         }
@@ -36,7 +74,23 @@ public class Database{
         return recipes.size();
     }
 
-    public List<String> getRecipe(int index){
-        return recipes.get(index);
+    public String getName(int index){
+        return name.get(index);
+    }
+
+    public int getTime(int index){
+        return time.get(index);
+    }
+
+    public Map<String, Double> getIngredients(int index){
+        return ingredients.get(index);
+    }
+
+    public List<String> getInstructions(int index){
+        return instructions.get(index);
+    }
+
+    public String getImage(int index){
+        return image.get(index);
     }
 }
